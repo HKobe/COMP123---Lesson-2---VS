1b8752e3127975c19cd6606ececb1bdde410cf89
HKobe <HKobeissi1@hotmail.com>
2016-05-17 12:54:39 -0400

    Initial Version

---
+++ COMP123 - S2016 - Lesson2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * @Author: Hassan Kobeissi
+ * @Date Created: May 5th, 2016
+ * @Date Modified: May 5th 2016
+ * @Description: Advanced Methods Demo for Lesson 2
+ * @Version: 0.0.1 - Initial Version
+ */
+
+namespace COMP123_S2016_Lesson2
+{
+ /*
+ * Main Driver Class for Lesson 2
+ * @Class Program 
+ */
+    class Program
+    {
+        /**
+         * Main method for our driver class
+         * @constructor Main
+         * @Param {string[]} args
+         */
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ UnitTestForLesson2/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using COMP123_S2016_Lesson2;
+
+namespace UnitTestForLesson2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arrange
+
+            //Act
+ 
+            //Assert
+
+        }
+    }
+}
