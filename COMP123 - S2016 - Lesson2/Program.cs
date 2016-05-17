8aa3ac6e3638033eb4314a7c229b94fc6fc2ae34
HKobe <HKobeissi1@hotmail.com>
2016-05-17 13:28:03 -0400

    Created/Added add method abd associated test

--- COMP123 - S2016 - Lesson2/Program.cs
+++ COMP123 - S2016 - Lesson2/Program.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
  * @Date Created: May 5th, 2016
  * @Date Modified: May 5th 2016
  * @Description: Advanced Methods Demo for Lesson 2
- * @Version: 0.0.1 - Initial Version
+ * @Version: 0.0.2 - Created/Added add method abd associated test
  */
 
 namespace COMP123_S2016_Lesson2
@@ -18,7 +18,7 @@ namespace COMP123_S2016_Lesson2
  * Main Driver Class for Lesson 2
  * @Class Program 
  */
-    class Program
+    public class Program
     {
         /**
          * Main method for our driver class
@@ -27,6 +27,24 @@ namespace COMP123_S2016_Lesson2
          */
         static void Main(string[] args)
         {
+            int x = 50;
+            int y = 40;
+            int result = 0;
+            result = add(x, y);
+            Console.WriteLine(result);
+        }
+        /*
+         * Simple Method to add two values
+         * @Method Add
+         * Values (x, y)
+         * Assossciated test
+         * 
+         * /
+       public static int add(int N1, int N2)
+        {
+            int result = 0;
+            result = N1 + N2;
+            return result;
         }
     }
 }
